2b16db953ca28e18debe316f269f697a5bef33f7
Elisa Mostafa <123586972+ElisaMostafa@users.noreply.github.com>
2025-03-24 20:01:11 -0300

    Create Pessoa.java
    Super classe Pessoa

---
+++ Pessoa.java
@@ -0,0 +1,37 @@
+
+package projeto2;
+
+
+public class Pessoa {
+    private String nome;
+    private int idade;
+
+    // Construtor
+    public Pessoa(String nome, int idade) {
+        this.nome = nome;
+        this.idade = idade;
+    }
+
+    // Getters e Setters
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    // Método para exibir informações
+    public void exibirDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+    }
+}
